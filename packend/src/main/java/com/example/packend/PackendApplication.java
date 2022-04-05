221949f3cd0c36c6d21df9b5491bf96b3e871212
Franzi <franziska.loof@stud.th-luebeck.de>
2022-04-05 13:08:24 +0200

    initial commit

---
+++ packend/src/main/java/com/example/packend/PackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.packend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PackendApplication.class, args);
+    }
+
+}
