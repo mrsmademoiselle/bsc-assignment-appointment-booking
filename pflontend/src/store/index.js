3016db84dd6b746a732777f759e4a97c49bf52fb
Franzi <franziska.loof@stud.th-luebeck.de>
2022-07-19 11:25:07 +0200

    added store for appointments

--- pflontend/src/store/index.js
+++ pflontend/src/store/index.js
@@ -1,8 +1,10 @@
 import {createStore} from 'vuex'
 import authentication from "@/store/modules/authentication";
+import appointments from "@/store/modules/appointments";
 
 export default createStore({
     modules: {
-        authentication
+        authentication,
+        appointments
     },
 })
---
+++ pflontend/src/store/modules/appointments.js
@@ -0,0 +1,66 @@
+import apiService from "@/services/ApiService";
+
+const state = () => ({
+    allAppointments: []
+})
+
+// getters
+const getters = {
+    appointments: state => {
+        return state.allAppointments;
+    }
+}
+
+const actions = { // asynchronous
+    async fetchAppointments({commit}, {token}) {
+        console.log("Fetching all appointments from server")
+
+        let response = await apiService.authenticatedGet("termin/get/all", token);
+
+        console.log("Response: '" + response.data + "' with Statuscode " + response.status);
+        if (response.status === 200) {
+            console.log("Fetch successful");
+            commit('fetch', response.data);
+        }
+    },
+    async removeAppointment({commit}, {id}, {token}) {
+        console.log("Remove appointment")
+
+        let response = await apiService.authenticatedPost("termin/cancel/" + id, {}, token);
+
+        console.log("Response: '" + response.data + "' with Statuscode " + response.status);
+        if (response.status === 200) {
+            console.log("Remove successful");
+            commit('remove', id);
+        }
+    },
+    async addAppointment({commit}, {appointment}) {
+        console.log("Add appointment");
+        let response = await apiService.authenticatedPost("termin/post", {appointment});
+        console.log("Response: '" + response.data + "' with Statuscode " + response.status);
+        if (response.status === 200) {
+            console.log("Add successful");
+            commit('add', appointment);
+        }
+    }
+}
+
+// mutations
+const mutations = { // synchronous
+    fetch(state, data) {
+        state.allAppointments = data;
+    },
+    remove(state, id) {
+        state.allAppointments = state.allAppointments.filter(app => app.id !== id);
+    },
+    add(state, appointment) {
+        state.allAppointments = state.allAppointments.push(appointment);
+    }
+}
+
+export default {
+    state,
+    mutations,
+    actions,
+    getters
+}
