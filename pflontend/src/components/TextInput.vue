6c9df34936174abe53e03a013ada075fb72f94c0
Franzi <franziska.loof@stud.th-luebeck.de>
2022-07-28 23:40:58 +0200

    fix textinput value equals input

--- pflontend/src/components/TextInput.vue
+++ pflontend/src/components/TextInput.vue
@@ -2,7 +2,7 @@
   <div class="form-group row">
     <label :for="this.label" class="col-3">{{ label }}:</label>
     <input :id="this.label" v-model="inputValue" :placeholder="this.placeholder" :type="this.type ? type : 'text'"
-           :value="input" class="form-control col-6"
+           class="form-control col-6"
            v-on:input="oninput">
   </div>
 </template>
@@ -13,7 +13,7 @@ export default {
   props: ['label', 'placeholder', 'type', 'input'],
   data: function () {
     return {
-      inputValue: null
+      inputValue: this.input
     }
   },
   methods: {
